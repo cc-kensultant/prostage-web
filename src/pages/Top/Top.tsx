8dd0680e56f05b179d7fedc14f3c5efad4e9287c
amano-m <ananotw@gmail.com>
2020-05-02 00:08:55 +0900

    不要コンポーネント削除

--- src/pages/Top/Top.tsx
+++ src/pages/Top/Top.tsx
@@ -1,50 +1,12 @@
 /** @jsx jsx */
 import { Helmet } from 'react-helmet'
-import { jsx, css } from '@emotion/core'
-import useSWR from 'swr'
-import { fetcher } from '../../utils/fetcher'
-import { Hello } from '../../components/Hello'
-
-export type SampleData = {
-  employee_name: string
-  employee_salary: number
-}
-
-const style = css`
-  color: red;
-`
+import { jsx } from '@emotion/core'
 
 export const Top = () => {
-  const { data } = useSWR('https://dummy.restapiexample.com/api/v1/employees', fetcher)
-
   return (
-    <div className="App">
-      <Helmet>
-        <title>Prostage | TOP</title>
-      </Helmet>
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          css={style}
-        >
-          Learn React
-        </a>
-      </header>
-      <ul>
-        {data &&
-          data.data.map((d: SampleData) => (
-            <li key={d.employee_name}>
-              <p>{d.employee_name}</p>
-              <p>{d.employee_salary}</p>
-            </li>
-          ))}
-      </ul>
-      <Hello name="top">
-        <p>Hello</p>
-      </Hello>
+    <div>
+      <Helmet title="トップページ | Prostage" />
+      トップページ
     </div>
   )
 }
