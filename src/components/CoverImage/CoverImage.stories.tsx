45df81379dcca2ddf8cb85c027ac351f57abc1eb
amano-m <ananotw@gmail.com>
2020-06-14 00:55:49 +0900

    カバー画像テスト修正

--- src/components/CoverImage/CoverImage.stories.tsx
+++ src/components/CoverImage/CoverImage.stories.tsx
@@ -1,18 +1,10 @@
 import React from 'react'
-
-/* eslint-disable */
-import { action } from '@storybook/addon-actions'
-/* eslint-disable */
-
 import { CoverImage } from './CoverImage'
+import Image from '../../images/CoverImage.svg'
 
 export default {
   title: 'CoverImage',
   component: CoverImage,
 }
 
-export const Default = () => (
-  <CoverImage imageSrc="hogehoge">
-    <p>dataX</p>
-  </CoverImage>
-)
+export const Default = () => <CoverImage imageSrc={Image} />
