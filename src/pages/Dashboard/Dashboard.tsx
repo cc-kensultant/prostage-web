69cf6275d5e3ab4ce329c76f20d0aee28fd58129
amano-m <ananotw@gmail.com>
2020-06-14 03:22:24 +0900

    カリキュラム　雛形の作成

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,12 +1,14 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core'
-import { FC, useState, useEffect } from 'react'
+import { FC, useState, useEffect, Fragment } from 'react'
 import { Link } from 'react-router-dom'
 import { CoverImageContainer } from '../../containers/CoverImage'
 import { UserIconContainer } from '../../containers/UserIcon'
+import BookIcon from '../../images/library_books-24px.svg'
 
 export const Dashboard: FC = () => {
   const [userName, setUserName] = useState('')
+  const [tabNum, setTabNum] = useState(0)
   useEffect(() => {
     // TODO: ログイン情報から、このユーザーの名前を取得する
     setUserName('道明寺 瑚太朗')
@@ -23,6 +25,25 @@ export const Dashboard: FC = () => {
           プロフィールの編集
         </Link>
         <h1 css={styles.userInfo.name}>{userName}</h1>
+      </div>
+      <div css={styles.tabs.base}>
+        <button
+          type="button"
+          css={tabNum === 0 ? styles.tabs.item_enable : styles.tabs.item_disable}
+          onClick={() => setTabNum(0)}
+        >
+          カリキュラム
+        </button>
+      </div>
+      <hr css={styles.hr} />
+      <div css={styles.content.base}>
+        {tabNum === 0 && (
+          <Fragment>
+            <h2 css={styles.achievement.title}>達成度</h2>
+            <h2 css={styles.continued.title}>前回の続き</h2>
+            <h2 css={styles.learning.title}>学習中のコース</h2>
+          </Fragment>
+        )}
       </div>
     </main>
   )
@@ -66,6 +87,79 @@ const styles = {
       font-weight: 500;
       font-size: 24px;
       line-height: 36px;
+      color: #10202d;
+    `,
+  },
+  tabs: {
+    base: css`
+      margin-top: 28px;
+      padding: 0 15vw;
+    `,
+    item_enable: css`
+      width: 138px;
+      height: 32px;
+      cursor: pointer;
+      padding: 0 0 0 24px;
+      font-family: Mplus 1p;
+      font-weight: normal;
+      font-size: 18px;
+      line-height: 27px;
+      color: #555555;
+      border: unset;
+      border-bottom: 4px solid #1890ff;
+      background: url("${BookIcon}") no-repeat;
+      outline: none;
+    `,
+    item_disable: css`
+      width: 138px;
+      height: 32px;
+      cursor: pointer;
+      padding: 0 0 0 24px;
+      font-family: Mplus 1p;
+      font-weight: normal;
+      font-size: 18px;
+      line-height: 27px;
+      color: #555555;
+      border: unset;
+      background: url("${BookIcon}") no-repeat;
+      outline: none;
+    `,
+  },
+  hr: css`
+    margin-top: unset;
+  `,
+  content: {
+    base: css`
+      padding: 0 15vw;
+    `,
+  },
+  achievement: {
+    title: css`
+      font-family: Mplus 1p;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 18px;
+      line-height: 27px;
+      color: #10202d;
+    `,
+  },
+  continued: {
+    title: css`
+      font-family: Mplus 1p;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 18px;
+      line-height: 27px;
+      color: #10202d;
+    `,
+  },
+  learning: {
+    title: css`
+      font-family: Mplus 1p;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 18px;
+      line-height: 27px;
       color: #10202d;
     `,
   },
