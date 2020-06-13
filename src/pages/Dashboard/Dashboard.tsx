0c3df63837d89e5950644d17da8d24e8d7cf0d8d
amano-m <ananotw@gmail.com>
2020-06-14 02:16:08 +0900

    名前・プロフィール編集ボタンの配置

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,17 +1,28 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core'
-import { FC } from 'react'
+import { FC, useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import { CoverImageContainer } from '../../containers/CoverImage'
 import { UserIconContainer } from '../../containers/UserIcon'
 
 export const Dashboard: FC = () => {
+  const [userName, setUserName] = useState('')
+  useEffect(() => {
+    // TODO: ログイン情報から、このユーザーの名前を取得する
+    setUserName('道明寺 瑚太朗')
+  }, [])
   return (
     <main css={styles.main}>
       <CoverImageContainer />
-      <div css={styles.userIcon.base}>
-        <div css={styles.userIcon.absolute}>
+      <div css={styles.userInfo.base}>
+        <div css={styles.userInfo.icon}>
           <UserIconContainer />
         </div>
+        {/* TODO: プロフィール編集画面へのリンクに差し替え */}
+        <Link to="/" css={styles.userInfo.link}>
+          プロフィールの編集
+        </Link>
+        <h1 css={styles.userInfo.name}>{userName}</h1>
       </div>
     </main>
   )
@@ -21,14 +32,41 @@ const styles = {
   main: css`
     width: 100vw;
   `,
-  userIcon: {
+  userInfo: {
     base: css`
-      position: relative;
+      padding: 0 15vw;
     `,
-    absolute: css`
-      position: absolute;
-      top: -80px;
-      left: 15vw;
+    icon: css`
+      margin-top: -80px;
+    `,
+    link: css`
+      margin-left: auto;
+      margin-top: -30px;
+      width: 190px;
+      height: 38px;
+      border: 1px solid #1890ff;
+      border-radius: 20px;
+      display: block;
+      text-align: center;
+      text-decoration: unset;
+      font-family: Mplus 1p;
+      font-style: normal;
+      font-weight: 500;
+      font-size: 14px;
+      line-height: 38px;
+      color: #096dd9;
+      &:hover,
+      &:focus {
+        outline: none;
+        background: #f3f3f3;
+      }
+    `,
+    name: css`
+      font-family: Mplus 1p;
+      font-weight: 500;
+      font-size: 24px;
+      line-height: 36px;
+      color: #10202d;
     `,
   },
 }
